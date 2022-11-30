736c241c07e4fa0e64360f5bbd3aa26833f51021
nameindustry <nameindustry@gmail.com>
2022-11-30 18:06:59 -0500

    [ update ] - ported guide and privacy to tailwinds and astro, removed assets folder and redundant files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'fira': ['Fira code'],
+			'montserrat': ['Montserrat']
+		}
 	},
 	plugins: [],
 }
